ec5c51e500a48a144e5d191fa875c6cc3bd58817
nobody <nobody@nowhere.net>
2026-04-15 18:39:13 -0700

    More aesthetic tuning: fade the spiral and change radial stretching.

--- app/src/spiral.rs
+++ app/src/spiral.rs
@@ -8,8 +8,10 @@ use crate::{Interval, SpiralProjector, Ticks, TryWidget, UnitCircleProjector};
 const SPIRAL_HOURS_PER_ROTATION: i32 = 24;
 const SPIRAL_ROTATIONS: i32 = 30;
 const TIME_WARP_POWER: f32 = 0.5;
-const SPIRAL_POINT_COUNT: usize = 10000;
-const SPIRAL_POINT_COUNT_F: f32 = SPIRAL_POINT_COUNT as f32;
+const SPIRAL_POINTS_PER_LEVEL: usize = 800;
+const SPIRAL_POINT_LEVELS: usize = 16;
+const SPIRAL_POINTS_TOTAL_F: f32 = (SPIRAL_POINTS_PER_LEVEL * SPIRAL_POINT_LEVELS) as f32;
+const SPIRAL_LEVEL_GAMMA_FACTOR: f32 = 0.6;
 const SPINE_COLOR: Color32 = Color32::from_gray(90);
 const TICK_COLOR: Color32 = Color32::from_gray(120);
 const TICK_ATTENUATION_FACTOR: f32 = 0.9;
@@ -37,15 +39,25 @@ impl TryWidget for SpiralWidget {
         let ucp = UnitCircleProjector::new(ui.max_rect())?;
         let spiral_stroke_width = 0.5;
 
-        let mut pts = vec![];
-        for i in 0..SPIRAL_POINT_COUNT {
-            let f = PositiveFinite::new((i as f32) / SPIRAL_POINT_COUNT_F)?;
-            let pt = self.sproj.project(f);
-            let pt = ucp.project(pt);
-            pts.push(pt);
-        }
+        for level in 0..SPIRAL_POINT_LEVELS {
+            let mut pts = vec![];
 
-        painter.line(pts, (spiral_stroke_width, SPINE_COLOR));
+            for lvli in 0..SPIRAL_POINTS_PER_LEVEL {
+                let i = level * SPIRAL_POINTS_PER_LEVEL + lvli;
+                let f = PositiveFinite::new((i as f32) / SPIRAL_POINTS_TOTAL_F)?;
+                let pt = self.sproj.project(f);
+                let pt = ucp.project(pt);
+                pts.push(pt);
+            }
+
+            painter.line(
+                pts,
+                (
+                    spiral_stroke_width,
+                    SPINE_COLOR.gamma_multiply(SPIRAL_LEVEL_GAMMA_FACTOR.powi(level as i32)),
+                ),
+            );
+        }
 
         let interval = Interval::new(
             self.now.clone(),
